e025d8c3e8f48e07634ea87975946417d625b4ac
NourAllah Ahmed <87453033+NourallahAhmed@users.noreply.github.com>
2024-07-25 08:57:55 +0300

    Initial Commit

---
+++ MovieAppTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieAppTask
+//
+//  Created by NourAllah Ahmed on 25/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
